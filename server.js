4feb49686829925185841fdee2c631ed6f459af2
Divansu <divansu@xcrino.com>
2025-06-01 14:25:03 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const io = require("socket.io")(8000,{
+    cors:"http://127.0.0.1:5500/View/index.html"
+})
+const users = {}
+
+io.on("connect",(socket)=>{
+    
+    socket.on("user-joined",(name)=>{
+        users[socket.id] = name
+        socket.broadcast.emit("new-user-joined",name)
+        console.log(users)
+    }) 
+
+    socket.on("send",(message)=>{
+        socket.broadcast.emit("receive",({name:users[socket.id],message}))
+    })
+    socket.on("disconnect",()=>{
+        let name = users[socket.id]
+        socket.broadcast.emit("user-left",name)
+    })
+})
+console.log("Server is running on port 8000")
